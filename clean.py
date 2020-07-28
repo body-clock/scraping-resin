1d7446898c5fec5579c1e05fa0dfc820d0e7415b
lilblizzard <lilblizzard97@gmail.com>
2020-07-28 15:17:20 -0400

    error handling for read timeout

--- clean.py
+++ clean.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv("output.csv")
+df = pd.read_csv("illinois.csv")
 df_clean = df.drop_duplicates(subset=['phone'])
-df_clean.to_csv("output_clean.csv")
+df_clean.to_csv("illinois_clean.csv")
--- scraping-resin.rb
+++ scraping-resin.rb
@@ -6,11 +6,26 @@ require 'openssl'
 require 'dotenv/load'
 
 def page_content(url)
-  Nokogiri::HTML(URI.open(url, 
+  attempt_count = 0
+  max_attempts  = 3
+  request_headers = {
     "User-Agent"                     => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0",
     :allow_redirections              => :all,
     :proxy_http_basic_authentication => [URI.parse("http://proxy.crawlera.com:8010"), ENV['CRAWLERA_KEY'], ""],
-    :ssl_verify_mode                 => OpenSSL::SSL::VERIFY_NONE))
+    :ssl_verify_mode                 => OpenSSL::SSL::VERIFY_NONE,
+    :read_timeout                    => 60
+  }
+  begin
+    attempt_count += 1
+    puts "Attempt: #{attempt_count}" if attempt_count > 1
+    content        = URI.open(url, request_headers)
+  rescue SocketError, Net::ReadTimeout => e
+    puts "Error: #{e}"
+    sleep 3
+    retry if attempt_count < max_attempts
+  else
+    Nokogiri::HTML(content)
+  end
 end
 
 def township_urls(state_html) 
@@ -34,26 +49,31 @@ def dispensary_data(dispensary_html)
   data
 end
 
-
-#TODO: allow user input
-state_url     = "https://weedmaps.com/dispensaries/in/united-states/michigan/"
+puts "Enter a state:"
+state_input   = gets
+state         = state_input.chomp.downcase
+state_url     = "https://weedmaps.com/dispensaries/in/united-states/#{state}/"
 state_content = page_content(state_url)
 township_urls = township_urls(state_content)
 
 headers = ["name", "phone", "email", "site"]
-CSV.open("output.csv", "a+") do |row|
+CSV.open("#{state}.csv", "a+") do |row|
   row << headers
 end
 
 for township_url in township_urls
+  puts township_url
   township_content = page_content(township_url)
   dispensary_urls  = dispensary_urls(township_content)
   for dispensary_url in dispensary_urls
+    puts "...#{dispensary_url}"
     dispensary_content = page_content(dispensary_url)
     data               = dispensary_data(dispensary_content)
     data_row           = [data["name"], data["phone"], data["email"], data["site"]]
-    CSV.open("output.csv", "a+") do |row|
+    CSV.open("#{state}.csv", "a+") do |row|
       row << data_row
     end
   end
 end
+
+puts "done!"
